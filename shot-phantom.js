7fddc2a00834e74f2c61d16fb1ebab7c53e279b5
Edy Segura <ednardo@inatel.br>
2017-11-03 14:13:40 -0200

    Updated shot-phantom.js script

--- shot-phantom.js
+++ shot-phantom.js
@@ -1,10 +1,14 @@
-const page = require('webpage').create()
+/**
+ * Bear in mind that PhamtonJS doesn't allow ES6+ features
+ */
+
+var page = require('webpage').create()
 
 page.open('http://edysegura.com', function () {
-    let title = page.evaluate(function () {
-        return document.title;
-    });
-    page.clipRect = { top: 0, left: 0, width: 600, height: 700 };
-    page.render(title + ".png");
-    phantom.exit();
+    var title = page.evaluate(function () {
+        return document.title
+    })
+    page.clipRect = { top: 0, left: 0, width: 600, height: 700 }
+    page.render('./screenshots/' + title + ".png")
+    phantom.exit()
 })
